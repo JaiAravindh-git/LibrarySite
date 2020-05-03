2b5f950f4318b579d2134622038de1a3885c484e
JaiAravindh-git <offl.aravindh@gmail.com>
2020-05-03 20:38:31 +0530

    centered view

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:parthalib/widgets/navigation_bar/navigation_bar.dart';
+import 'package:parthalib/widgets/centered_view/centered_view.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children:<Widget>[
-          NavigationBar()
-        ]
+      body: CenteredView(
+              child: Column(
+          children:<Widget>[
+            NavigationBar()
+          ]
+        ),
       ) ,
     );
   }
