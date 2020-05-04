dcfa2e3fd2ae49c8b5d1069466de9a1e1eeec47c
JaiAravindh-git <offl.aravindh@gmail.com>
2020-05-04 10:41:09 +0530

    navbar refactor

--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,32 +1,17 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:parthalib/widgets/navigation_bar/navigation_bar_mobile.dart';
+import 'package:parthalib/widgets/navigation_bar/navigation_bar_tablet_desktop.dart';
+
 
 class NavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height:100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children:<Widget> [
-          SizedBox(
-            height: 80,
-            width: 150,
-            child: Image.asset('assets/logo.png'),
-          ),
-          Row(
-            mainAxisSize : MainAxisSize.min,
-            children: <Widget>[   
-            NavBarItem('Episodes'), 
-            SizedBox(
-              width:60,
-            ),
-            NavBarItem('About'),
-          ]),
-          
-        ]
-      )
-    );
+    return ScreenTypeLayout(
+      mobile : NavigationBarMobile(),
+      tablet: NavigationBarTabletDesktop(),
+    )
   }
 }
 
