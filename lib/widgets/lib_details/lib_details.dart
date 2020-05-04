93951d4d1222de44214a95e8fa041391d2b8cdf7
JaiAravindh-git <offl.aravindh@gmail.com>
2020-05-04 11:41:35 +0530

    small update

--- lib/widgets/lib_details/lib_details.dart
+++ lib/widgets/lib_details/lib_details.dart
@@ -4,7 +4,6 @@ import 'package:responsive_builder/responsive_builder.dart';
 class LibDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return ResponsiveBuilder(builder: (context, sizingInformation) {
       var textAlignment =
           sizingInformation.deviceScreenType == DeviceScreenType.Desktop
@@ -17,25 +16,35 @@ class LibDetails extends StatelessWidget {
               : 80;
       double descriptionSize = sizingInformation.deviceScreenType == DeviceScreenType.Mobile ?
       16 : 21;
-    
-    return Container(
-      width: 600,
-      child: Column(
+      return Container(
+        width: 600,
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'PSNA LIBRARY. \nFOR PARTHA',
-              style: TextStyle(fontWeight: FontWeight.w800, height: 0.9, fontSize: titleSize),
+              'PSNA LIBRARY.\nFOR PARTHA',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                height: 0.9,
+                fontSize: titleSize,
+              ),
               textAlign: textAlignment,
             ),
-            SizedBox(height: 30),
+            SizedBox(
+              height: 30,
+            ),
             Text(
-              'welcome to the library management portal. This was made \nin remembrance of Mr.Parthasarathy who had a vision of \nhaving a e-portal for college unfortunateley he passed \naway yet a student wants to carry his dream',
-              style: TextStyle(fontSize: 21, height: 1.7),
+              'This was a vision of a technician who had a vision of building a library portal for college, for students.',
+              style: TextStyle(
+                fontSize: descriptionSize,
+                height: 1.7,
+              ),
               textAlign: textAlignment,
             )
-          ]),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
