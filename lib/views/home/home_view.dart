82d7f798ecd8cdf610b1819ebbc65eac7193c229
JaiAravindh-git <offl.aravindh@gmail.com>
2020-05-04 12:39:44 +0530

    sidebar done

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:parthalib/nav_drawer/navigation_drawer_header.dart';
 import 'package:parthalib/widgets/navigation_bar/navigation_bar.dart';
 import 'package:parthalib/widgets/centered_view/centered_view.dart';
 import 'package:parthalib/widgets/home/home_content_desktop.dart';
 import 'package:parthalib/widgets/home/home_content_mobile.dart';
-
+import 'package:parthalib/nav_drawer/navigation_drawer.dart';
 
 import 'package:responsive_builder/responsive_builder.dart';
 
@@ -12,14 +13,17 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ResponsiveBuilder(
       builder: (context, sizingInformation) =>
-          Scaffold(
+        Scaffold(
+          drawer: sizingInformation.deviceScreenType == DeviceScreenType.Mobile ? NavigationDrawer() : null ,
         backgroundColor: Colors.white,
         body: CenteredView(
-          child: Column(children: <Widget>[
-            NavigationBar(),
-           Expanded(child: ScreenTypeLayout(
-             mobile: HomeContentMobile(),
-             desktop: HomeContentDesktop(),
+         child: Column(
+         children: <Widget>[
+           NavigationBar(),
+           Expanded(
+             child: ScreenTypeLayout(
+              mobile: HomeContentMobile(),
+              desktop: HomeContentDesktop(),
            ),)
           ]),
         ),
